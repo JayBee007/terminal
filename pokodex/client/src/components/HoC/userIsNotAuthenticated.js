e811130c53a19b026a0a50890321f77a09ceab20
Jay Bee <jaybee.codes@gmail.com>
2018-04-01 11:22:10 +0300

    add redux react-router sass-loader

---
+++ pokodex/client/src/components/HoC/userIsNotAuthenticated.js
@@ -0,0 +1,17 @@
+import locationHelperBuilder from 'redux-auth-wrapper/history4/locationHelper'
+import { connectedRouterRedirect } from 'redux-auth-wrapper/history4/redirect';
+
+const locationHelper = locationHelperBuilder({})
+
+const userIsNotAuthenticated = connectedRouterRedirect({
+    redirectPath: (state, ownProps) => locationHelper.getRedirectQueryParam(ownProps) || '/pokemons',
+
+    allowRedirectBack: false,
+
+    authenticatedSelector: state => state.auth.auth === false,
+    //authenticatedSelector: state=>false,
+
+    wrapperDisplayName: 'UserIsNotAuthenticated'
+});
+
+export default userIsNotAuthenticated;
