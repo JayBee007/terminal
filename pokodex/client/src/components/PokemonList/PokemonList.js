f9bbbe8e8ae351251add575dcdb7930453ba0e5b
Jay Bee <jaybee.codes@gmail.com>
2018-04-07 23:29:38 +0300

    add search by name,ability, type and filter by tags

--- pokodex/client/src/components/PokemonList/PokemonList.js
+++ pokodex/client/src/components/PokemonList/PokemonList.js
@@ -27,26 +27,55 @@ class PokemonList extends React.Component {
     }));
   }
 
+  searchFilter = (filterString) => {
+    const regex = new RegExp(filterString, 'gi');
+    const filteredPokemon = this.props.pokemons.data.filter(pokemon => {
+      return pokemon.name.match(regex) || pokemon.abilities.some(ability => ability.match(regex)) || pokemon.types.some(type => type.match(regex));
+    });
+
+    this.setState(() => ({
+      pokemons: filteredPokemon
+    }));
+  }
+
+  filterByTags = (filterString) => {
+
+    const regex = new RegExp(filterString, 'gi');
+    const filterPokemon = this.props.pokemons.data.filter(pokemon => {
+      return pokemon.abilities.some(ability => ability.match(regex)) || pokemon.types.some(type => type.match(regex));
+    });
+    this.setState(() => ({
+      pokemons: filterPokemon
+    }));
+  }
+
   render() {
     const { pokemons } = this.props;
     const { requesting, successful, errors } = pokemons;
       return (
         <section className="list">
-          <Search />
+        <div className="list__meta">
+          <Search filterPokemon={this.searchFilter} />
+        </div>
+
         {
           requesting &&
           <Loader isLoading={requesting} />
         }
-        { //successful &&
-          // this.state.pokemons.map(pokemon => (
-          //   <Card key={pokemon.id}
-          //         avatar={pokemon.image}
-          //         name={pokemon.name}
-          //         abilities={pokemon.abilities}
-          //         types={pokemon.types}
-          //         id={pokemon.id} />
-          // ))
+        <div className="list__card">
+        { successful &&
+          this.state.pokemons.map(pokemon => (
+            <Card key={pokemon.id}
+                   avatar={pokemon.image}
+                   name={pokemon.name}
+                   abilities={pokemon.abilities}
+                   types={pokemon.types}
+                   id={pokemon.id}
+                   filterByTags={this.filterByTags} />
+           ))
         }
+        </div>
+
         {
           errors &&
           <Error name={errors.name} message={errors.message} />
--- pokodex/client/src/components/Search/Search.js
+++ pokodex/client/src/components/Search/Search.js
@@ -3,15 +3,14 @@ import React from 'react';
 const Search = (props) => {
 
   const handleOnChange = (e) => {
-    console.log(e.target.value);
+    e.preventDefault();
+    props.filterPokemon(e.target.value);
   }
 
   return(
-    <div className="search">
-      <form className="search__form">
+      <form className="search">
         <input className="search__input" type="text" onChange={handleOnChange} />
       </form>
-    </div>
   )
 }
 
