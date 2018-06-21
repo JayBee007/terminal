3ce0e476b8fea651f21c35f4640dd9ea11925e9c
Jay Bee <jaybee.codes@gmail.com>
2018-06-21 18:24:38 +0300

    add post route /campground/new

--- yelpcamp/app.js
+++ yelpcamp/app.js
@@ -1,10 +1,17 @@
 import express from "express";
 import morgan from "morgan";
+import bodyParser from "body-parser";
 
+let campgrounds = [
+  {name: "Salmon Creek", image: "https://pixabay.com/get/eb3db30a29fd063ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
+  {name: "Granite Hill", image: "https://pixabay.com/get/e833b3092cf5033ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db80d2cfd053ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
+]
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -13,13 +20,18 @@ app.get("/", (req,res) => {
 });
 
 
+app.post("/campgrounds", (req,res) => {
+  campgrounds.push(req.body);
+  res.redirect("campgrounds");
+});
+
+
 app.get("/campgrounds", (req,res) => {
-  const campgrounds = [
-    {name: "Salmon Creek", image: "https://pixabay.com/get/eb3db30a29fd063ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
-    {name: "Granite Hill", image: "https://pixabay.com/get/e833b3092cf5033ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
-    {name: "Mountain Goat's Rest", image: "https://pixabay.com/get/e83db80d2cfd053ed1584d05fb1d4e97e07ee3d21cac104497f9c471a7eab0bd_340.jpg"},
-  ]
   res.render("campgrounds", { campgrounds });
+});
+
+app.get("/campgrounds/new", (req,res) => {
+  res.render("new");
 });
 
 
