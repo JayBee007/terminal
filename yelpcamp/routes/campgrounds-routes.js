7a7ab7c06fd1fc48cba275c03cd245b5d43eb047
Jay Bee <jaybee.codes@gmail.com>
2018-06-23 12:06:12 +0300

    add eslint

--- yelpcamp/routes/campgrounds-routes.js
+++ yelpcamp/routes/campgrounds-routes.js
@@ -1,33 +1,42 @@
-import express from "express";
+/* eslint array-callback-return: 0 */
+
+import express from 'express';
+
+import Campground from '../models/campground';
+
 const router = express.Router();
 
-import Campground from "../models/campground.js";
-
-router.get("/", (req,res) => {
-  console.log("/campgrounds get")
+router.get('/', (req, res) => {
   Campground.find((err, campgrounds) => {
-    if(err) res.json(err);
-    res.render("campgrounds", { campgrounds });
-  })
+    if (err) res.json(err);
+    res.render('index', { campgrounds });
+  });
 });
 
 
-router.post("/", (req,res) => {
-  const { name, image } = req.body;
+router.post('/', (req, res) => {
+  const { name, image, description } = req.body;
   const campground = new Campground({
     name,
-    image
+    image,
+    description,
   });
-  campground.save((err, campground) => {
-    if(err) console.log(err);
-    res.redirect("campgrounds");
-  })
-
+  campground.save((err) => {
+    if (err) res.send(err).status(500);
+    res.redirect('campgrounds');
+  });
 });
 
+router.get('/new', (req, res) => {
+  res.render('new');
+});
 
-router.get("/new", (req,res) => {
-  res.render("new");
+router.get('/:id', (req, res) => {
+  // Adventure.findById(id, function (err, adventure) {});
+  Campground.findById(req.params.id, (err, campground) => {
+    if (err) res.send(err).status(500);
+    res.render('show', { campground });
+  });
 });
 
 
