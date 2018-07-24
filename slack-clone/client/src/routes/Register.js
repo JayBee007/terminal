996f248491aeb8fd1677ce603863b52440accc89
Jay Bee <jaybee.codes@gmail.com>
2018-07-24 23:29:53 +0300

    add basic client side register form validation

--- slack-clone/client/src/routes/Register.js
+++ slack-clone/client/src/routes/Register.js
@@ -1,3 +1,4 @@
+/* eslint no-useless-escape:0 */
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -23,17 +24,52 @@ class Register extends React.Component {
     username: '',
     email: '',
     password: '',
+    emailError: '',
+    passwordError: '',
+    usernameError: '',
   }
 
   handeOnChange = (e) => {
     const { id, value } = e.target;
+
+    switch(id) {
+      case 'email':
+        const regex = /^([A-Za-z0-9_\-\.]){1,}\@([A-Za-z]){1,}\.([a-z]{2,4})$/
+        if (!regex.test(value)) {
+          this.setState(() => ({
+            [`${id}Error`]: 'invalid e-mail'
+          }))
+          return;
+        }
+        break;
+      case 'username':
+        if (value.length < 3) {
+          this.setState(() => ({
+            [`${id}Error`]: 'invalid username'
+          }))
+          return
+        }
+        break;
+      case 'password':
+        if (value.length < 6) {
+          this.setState(() => ({
+            [`${id}Error`]: 'password must be minimum 6 characters'
+          }))
+          return
+        }
+        break;
+      default:
+        break;
+    }
     this.setState(() => ({
-      [id]: value
+      [id]: value,
+      [`${id}Error`]: ''
     }))
   }
 
   render() {
-    const { classes } = this.props
+    const { classes } = this.props;
+    const { emailError, usernameError, passwordError, email, username, password } = this.state;
     return (
       <Mutation mutation={REGISTER} update={({data}) => {console.log(data)}}>
       {(register) => (
@@ -42,35 +78,46 @@ class Register extends React.Component {
             <Card className={classes.root}>
               <Typography variant="headline" align="center">Register</Typography>
               <form
+                autoComplete="off"
                 onSubmit={e => {
                   e.preventDefault();
-                  register({variables:this.state})
+                  register({variables:{
+                    username,
+                    email,
+                    password
+                  }})
                 }}
               >
                 <TextField
+                  error={usernameError.length > 0}
                   fullWidth
                   id="username"
                   label="Username"
                   margin="normal"
                   required
+                  helperText={usernameError}
                   onChange={this.handeOnChange}
                 />
                 <TextField
+                  error={emailError.length > 0}
                   fullWidth
                   type="email"
                   id="email"
                   label="Email"
                   margin="normal"
                   required
+                  helperText={emailError}
                   onChange={this.handeOnChange}
                 />
                 <TextField
+                  error={passwordError.length > 0}
                   fullWidth
                   type="password"
                   id="password"
                   label="Password"
                   margin="normal"
                   required
+                  helperText={passwordError}
                   onChange={this.handeOnChange}
                 />
                 <Button type="submit" variant="contained" color="primary" fullWidth>
