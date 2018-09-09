1454bc241528c84ba008369f4bc4ec45c2cee090
Javed <jaybee.codes@gmail.com>
2018-09-09 21:20:32 +0300

    initial setup subscriptions on client and server

--- slack-clone/client/src/apollo.js
+++ slack-clone/client/src/apollo.js
@@ -1,4 +1,11 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { withClientState } from 'apollo-link-state';
+import { ApolloLink, Observable } from 'apollo-link';
 
 import { getUserFromLocalStorage } from './services/authService';
 
@@ -6,24 +13,84 @@ import defaults from './state/defaults';
 import resolvers from './state/resolvers';
 import typeDefs from './state/typeDefs';
 
-const client = new ApolloClient({
+const cache = new InMemoryCache();
+
+const httpLink = new HttpLink({
   uri: 'http://localhost:4000/graphql',
-  request:(operation) => {
-    const user  = getUserFromLocalStorage();
-    let headers;
-    if(user) {
-      headers = {
-        'x-token': JSON.parse(user).token
-      }
-      operation.setContext({headers})
-    }
-  },
-  clientState: {
-    defaults,
-    resolvers,
-    typeDefs
+
+})
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/subscriptions`,
+  options: {
+    reconnect: true
   }
 });
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLink,
+);
+
+const request = operation => {
+  const user  = getUserFromLocalStorage();
+  let headers;
+  if(user) {
+    headers = {
+      'x-token': JSON.parse(user).token
+    }
+    operation.setContext({headers})
+  }
+}
+
+const requestLink = new ApolloLink((operation, forward) =>
+  new Observable(observer => {
+    let handle;
+    Promise.resolve(operation)
+      .then(oper => request(oper))
+      .then(() => {
+        handle = forward(operation).subscribe({
+          next: observer.next.bind(observer),
+          error: observer.error.bind(observer),
+          complete: observer.complete.bind(observer),
+        });
+      })
+      .catch(observer.error.bind(observer));
+
+    return () => {
+      if (handle) handle.unsubscribe();
+    };
+  })
+);
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    withClientState({
+      defaults,
+      resolvers,
+      typeDefs,
+      cache,
+    }),
+    requestLink,
+    link
+  ]),
+  cache,
+})
+
+// const client = new ApolloClient({
+//   uri: 'http://localhost:4000/graphql',
+//   request:(operation) => ,
+//   clientState: {
+//     defaults,
+//     resolvers,
+//     typeDefs
+//   }
+// });
 
 
 export default client;
