d3d816edef710099f31661fa7635784c4392acc7
Javed <jaybee.codes@gmail.com>
2018-09-12 00:31:09 +0300

    finish susbscriptions :fire:

--- slack-clone/client/src/components/Messages/Messages.js
+++ slack-clone/client/src/components/Messages/Messages.js
@@ -1,33 +1,29 @@
 import React from 'react';
-import { Query } from 'react-apollo';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles'
 
 import Message from './Message';
-import Loader from '../Loader';
 
-import { GET_MESSAGES } from '../../services/messageService';
+class Messages extends React.Component {
 
+  componentDidMount() {
+    this.props.subscribeToNewMessages();
+  }
 
-const Messages = (props) => (
-  <Query query={GET_MESSAGES} variables={{channelId: props.channelId}}>
-  {({ loading, error, data: {getMessages }}) => {
-
-    if(loading) return <Loader />
-    if(error) return <p>Error: {error}</p>
-
+  render() {
+    const { messages, classes } = this.props;
     return (
-      <Grid container direction='column' className={props.classes.container} >
+      <Grid container direction='column' className={classes.container} >
         {
-          getMessages.map(message => (
+          messages.map(message => (
             <Message key={message.id} {...message} />
           ))
         }
       </Grid>
     )
-  }}
-  </Query>
-)
+  }
+}
+
 
 const styles = {
   container: {
